104beca89b048ae957f4a1d6c750b28c2ce80941
darenf <daren@digcat.com>
2015-04-19 07:26:06 +0100

    add a script to call makecontent to help investigate cmislib reliability and generate load for datavis

---
+++ datavis-repo/src/test/runload.sh
@@ -0,0 +1,5 @@
+python makecontent.py http://localhost:8080/alfresco/cmisatom site1 50 N > /dev/null &
+python makecontent.py http://localhost:8080/alfresco/cmisatom site2 50 N > /dev/null &
+python makecontent.py http://localhost:8080/alfresco/cmisatom site3 50 N > /dev/null &
+python makecontent.py http://localhost:8080/alfresco/cmisatom site4 50 N > /dev/null &
+python makecontent.py http://localhost:8080/alfresco/cmisatom site5 50 N > /dev/null &
